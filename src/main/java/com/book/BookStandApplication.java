e1681a856e36a9a20d30bd05ab3b5225ae30c170
panguibao <1460900748@qq.com>
2020-12-17 14:15:35 +0800

    gradle 文件修改

--- src/main/java/com/book/BookStandApplication.java
+++ src/main/java/com/book/BookStandApplication.java
@@ -1,9 +1,11 @@
 package com.book;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.book.mapper")
 public class BookStandApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookStandApplication.class, args);
