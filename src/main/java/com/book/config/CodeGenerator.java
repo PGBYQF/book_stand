6d3a93c669092f3f8887a7ae0e704b0f38993450
panguibao <1460900748@qq.com>
2020-12-18 10:13:16 +0800

    不要启动执行

--- src/main/java/com/book/config/CodeGenerator.java
+++ src/main/java/com/book/config/CodeGenerator.java
@@ -6,7 +6,7 @@ import com.baomidou.mybatisplus.generator.InjectionConfig;
 import com.baomidou.mybatisplus.generator.config.*;
 import com.baomidou.mybatisplus.generator.config.po.TableInfo;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
-import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;` `
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 
 import java.util.ArrayList;
 import java.util.List;
